4fa31f82021fe655268fa1a3d0ad2535cd1929b4
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-02-09 11:11:29 +0200

    Solution settup

---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-CGU4KCR;Database=DoctorWhoCore;Trusted_Connection=True");
+        }
+    }
+}
